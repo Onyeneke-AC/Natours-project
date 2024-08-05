d4b6fa6b4ae5056800e1fd6f4e4e1933db2bb916
Onyeneke Anthony Chidubem <98026459+Onyeneke-AC@users.noreply.github.com>
2024-08-05 05:32:16 +0100

    aggragation pipeline & virtual properties

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -8,9 +8,17 @@ const {
   updateTour,
   deleteTour,
   aliasTopTours,
+  getTourStats,
+  getMonthlyPlan,
 } = tourController;
 
 const router = express.Router();
+
+// route for getting tour stats
+router.route('/monthly-plan/:year').get(getMonthlyPlan);
+
+// route for getting tour stats
+router.route('/tour-stats').get(getTourStats);
 
 // route to get the top 5 cheap tours
 router.route('/top-5-cheap').get(aliasTopTours, getAllTours);
