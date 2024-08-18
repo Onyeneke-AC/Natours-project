f1c04f87b86005ea7afdf9d9eb7407e316d0536e
Onyeneke Anthony Chidubem <98026459+Onyeneke-AC@users.noreply.github.com>
2024-08-18 11:30:34 +0100

    First steps with pug

--- app.js
+++ app.js
@@ -73,7 +73,10 @@ app.use((req, res, next) => {
 //3) ROUTES
 
 app.get('/', (req, res) => {
-  res.status(200).render('base');
+  res.status(200).render('base', {
+    tour: 'The Forest Hiker',
+    user: 'Jonas',
+  });
 });
 
 // app.get('/api/v1/tours', getAllTours);
