657ec941099704ab0b5fbac90c2d625471745df0
Onyeneke Anthony Chidubem <98026459+Onyeneke-AC@users.noreply.github.com>
2024-08-20 22:42:07 +0100

    Building the login screen

--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -3,10 +3,12 @@ const viewController = require('../controllers/viewController');
 
 const router = express.Router();
 
-const { getOverview, getTour } = viewController;
+const { getOverview, getTour, getLoginForm } = viewController;
 
 router.get('/', getOverview);
 
 router.get('/tour/:slug', getTour);
+
+router.get('/login', getLoginForm);
 
 module.exports = router;
